23fab262bdf1baf499cf8712509517e43f5d2b54
Javohir Uralov <uralovjavohir25@gmail.com>
2022-07-20 16:13:06 +0500

    @ParameterizedTest

---
+++ src/test/java/BankAccountParametrizedTestTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import java.time.DayOfWeek;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(BankAccountParameterResolver.class)
+public class BankAccountParametrizedTestTest {
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 400, 800, 1000})
+    @DisplayName("Depositing successfully")
+    public void testDeposit(int amount, BankAccount bankAccount) {
+        bankAccount.deposit(amount);
+        assertEquals(amount, bankAccount.getBalance());
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = DayOfWeek.class, names = {"TUESDAY", "THURSDAY"})
+    public void testDayOfWeek(DayOfWeek day){
+        assertTrue(day.toString().startsWith("T"));
+    }
+
+    @ParameterizedTest
+//    @CsvSource({"100, Mary", "200, Richard", "150, Ted"})
+    @CsvFileSource(resources = "details.csv")
+    public void depositAndNameTest(double amount, String name, BankAccount bankAccount){
+        bankAccount.deposit(amount);
+        bankAccount.setHolderName(name);
+        assertEquals(amount, bankAccount.getBalance());
+        assertEquals(name, bankAccount.getHolderName());
+    }
+
+}
